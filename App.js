6cef6f9b2d2a14961927b46626ca0a4f371c11b1
TbreezyF <edwin.ifionu@gmail.com>
2018-08-06 08:23:51 -0600

    'dz-v.3'

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ const cookie = require('cookie');
 const nonce = require('nonce')();
 const querystring = require('querystring');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const request = require('request-promise');
 const chargeMerchant = require('./scripts/createCharge.js');
 const gapi = require('./scripts/gCompute.js');
@@ -36,8 +35,12 @@ app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(cookieParser());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 AWS.config.update({
     region: "ca-central-1",
@@ -508,7 +511,7 @@ app.post('/api/hooks/', verifyToken, async(req, res) => {
     }
 });
 
-app.get('/api/optimize/', verifyToken, cors(), async(req, res) => {
+app.get('/api/optimize/', verifyToken, async(req, res) => {
     if (req.query.url) {
         onflyOptimize.process(req.query.url)
             .then(data => {
